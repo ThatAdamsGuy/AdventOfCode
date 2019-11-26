27afc1b2fa6b0f84ad11c33013db3b6528dbbaae
Harry Adams <Harry.Adams@aberinstruments.local>
2019-11-26 16:00:13 +0000

    2018, Day One

---
+++ 2018/1/2018_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _2018_1
+{
+    class Program
+    {
+        static string line;
+        static int frequency = 0;
+        static List<int> occurred = new List<int>();
+        static int? firstDuplicate;
+        static bool duplicateFound = false;
+
+        static void Main(string[] args)
+        {
+            int? result = RunThroughList();
+        }
+
+        private static int? RunThroughList()
+        {
+            using (TextReader tx = new StreamReader(@"input.txt"))
+            {
+                //Run through every line in file
+                while ((line = tx.ReadLine()) != null)
+                {
+                    Console.Write(line);
+                    int value = int.Parse(line);
+                    frequency += value;
+
+                    if (!duplicateFound && occurred.Contains(frequency))
+                    {
+                        firstDuplicate = frequency;
+                        duplicateFound = true;
+                    }
+                    else
+                    {
+                        occurred.Add(frequency);
+                    }
+                    Console.WriteLine(" - " + frequency);
+                }
+                Console.WriteLine("Final Frequency: " + frequency);
+                Console.WriteLine("First Duplicate: " + firstDuplicate);
+                Console.WriteLine();
+            }
+
+            if (firstDuplicate == null)
+            {
+                return RunThroughList();
+            } else
+            {
+                return firstDuplicate;
+            }
+
+        }
+    }
+}
