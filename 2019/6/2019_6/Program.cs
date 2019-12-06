45c2a3f8028fb2bfbc769e06a99138544e5e4e55
thatadamsguy <hyperionharry@gmail.com>
2019-12-06 13:10:53 +0000

    2019, Day 6 - UNF

---
+++ 2019/6/2019_6/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2019_6
+{
+    class Program
+    {
+        static List<KeyValuePair<string, string>> inputs;
+        static void Main(string[] args)
+        {
+            using (TextReader tx = new StreamReader(@"input.txt"))
+            {
+                inputs = new List<KeyValuePair<string, string>>();
+                string line;
+                while ((line = tx.ReadLine()) != null)
+                {
+                    string[] splitInput = line.Split(')');
+                    inputs.Add(new KeyValuePair<string, string>(splitInput[0], splitInput[1]));
+                }
+            }
+
+            int orbits = 0;
+            int counter = 1;
+
+            //foreach (var each in inputs)
+            //{
+            //    orbits += GetOrbits(each, 1);
+            //    counter++;
+            //}
+
+            Console.WriteLine("TOTAL Orbits: " + orbits);
+            Console.WriteLine("Orbits between YOU and SAN: " + JumpToSanta());
+        }
+
+        static int GetOrbits(KeyValuePair<string, string> input, int counter)
+        {
+            if (input.Key == "COM")
+            {
+                return counter;
+            }
+
+            var matchingRecord = inputs.FirstOrDefault(t => t.Value.Equals(input.Key));
+            if (matchingRecord.Key == "COM")
+            {
+                counter++;
+                return counter;
+            } else
+            {
+                counter++;
+                return GetOrbits(matchingRecord, counter);
+            }
+        }
+
+        static int JumpToSanta()
+        {
+            var YOU = inputs.FirstOrDefault(t => t.Value.Equals("YOU"));
+            var SAN = inputs.FirstOrDefault(t => t.Value.Equals("SAN"));
+
+
+            Console.Write("Planets from me to COM: ");
+            List<string> MeToCOM = GetPlanets("YOU", new List<string>());
+            List<string> SanToCOM = GetPlanets("SAN", new List<string>());
+            Console.WriteLine("Me to COM: " + MeToCOM.Count);
+            Console.WriteLine("Santa to COM: " + SanToCOM.Count);
+            HashSet<string> uniques = new HashSet<string>(MeToCOM.Union(SanToCOM));
+
+            Console.WriteLine("MY PLANETS: ");
+            int counter = 0;
+            foreach(string item in MeToCOM)
+            {
+                Console.WriteLine(counter + " - " + item);
+                counter++;
+            }
+            Console.WriteLine("SANTA'S PLANETS: ");
+            counter = 0;
+            foreach(string item in SanToCOM)
+            {
+                Console.WriteLine(counter + " - " + item);
+                counter++;
+            }
+
+            return uniques.Count;
+        }
+
+        static List<string> GetPlanets(string input, List<string> results)
+        {
+            var matchingRecord = inputs.FirstOrDefault(t => t.Value.Equals(input));
+
+            if (matchingRecord.Key == "COM")
+            {
+                results.Add(matchingRecord.Key);
+                return results;
+            }
+            else
+            {
+                results.Add(matchingRecord.Key);
+                return GetPlanets(matchingRecord.Key, results);
+            }
+        }
+    }
+}
