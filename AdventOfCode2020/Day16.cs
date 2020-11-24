dfef9391d39f766343775c535cead4b2d73f6d30
Harry Adams <harry.adams@aberinstruments.com>
2020-11-24 09:51:08 +0000

    Initial Project Commit for 2020

---
+++ AdventOfCode2020/Day16.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    class Day16
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
