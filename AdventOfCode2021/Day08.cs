8e9292c1d80274d8637a0b1f5fdc427d2b60bcfe
Harry Adams <hyperionharry@gmail.com>
2021-12-08 22:31:57 +0000

    2021, Day 8

--- AdventOfCode2021/Day08.cs
+++ AdventOfCode2021/Day08.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,146 @@ namespace AdventOfCode2021
     {
         public static void Run()
         {
+            long counter = 0;
+            var inputs = File.ReadAllLines("Day08Input.txt");
+            foreach(var item in inputs)
+            {
+                var outputs = item.Split('|')[1].Split(' ');
+                foreach (var output in outputs)
+                {
+                    if (output.Length == 2 || output.Length == 3 || output.Length == 4 || output.Length == 7)
+                        counter++;
+                }
+            }
+            Console.WriteLine("Part 1 - " + counter);
 
+            counter = 0;
+            string number = "";
+            foreach(var item in inputs)
+            {
+                var toCheck = item.Split('|')[1].Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+                var allDisplays = new List<string>(toCheck);
+                allDisplays.AddRange(item.Split('|')[0].Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).ToList());
+
+                if(toCheck.Count() != 4)
+                {
+                    throw new Exception();
+                }
+
+                string one = allDisplays.Where(s => s.Length == 2).FirstOrDefault();
+                string four = allDisplays.Where(s => s.Length == 4).FirstOrDefault();
+
+                if(one is null || four is null)
+                {
+                    throw new Exception();
+                }
+
+
+                foreach(var checking in toCheck)
+                {
+                    switch (checking.Length)
+                    {
+                        case 2:
+                            number += "1";
+                            Console.Write("1");
+                            break;
+                        case 3:
+                            number += "7";
+                            Console.Write("7");
+                            break;
+                        case 4:
+                            number += "4";
+                            Console.Write("4");
+                            break;
+                        case 7:
+                            number += "8";
+                            Console.Write("8");
+                            break;
+                        case 6:
+                            bool valid = true;
+                            foreach (char c in one)
+                            {
+                                if (!checking.Contains(c))
+                                {
+                                    valid = false;
+                                    break;
+                                }
+                            }
+                            if (valid)
+                            {
+                                number += "9";
+                                Console.Write("9");
+                                break;
+                            }
+                            else
+                            {
+                                number += "6";
+                                Console.Write("6");
+                            }
+                            break;
+                        case 5:
+                            valid = true;
+                            foreach(char c in one)
+                            {
+                                if(!checking.Contains(c))
+                                    valid = false;
+                            }
+                            if (valid)
+                            {
+                                number += "3";
+                                Console.Write("3");
+                                break;
+                            }
+
+                            if(four is null)
+                            {
+                                Console.WriteLine("Work to do, sorry.");
+                                break;
+                            }
+
+                            //Get Four without One mask
+                            string mask = four;
+                            foreach (char c in one)
+                            {
+                                if (mask.Contains(c)) {
+                                    mask = mask.Replace(c.ToString(), String.Empty);
+                                }
+                            }
+
+                            valid = true;
+                            foreach (char c in mask)
+                            {
+                                if (!checking.Contains(c))
+                                {
+                                    valid = false;
+                                    break;
+                                }
+                            }
+                            if (valid)
+                            {
+                                number += "5";
+                                Console.Write("5");
+                                break;
+                            }
+
+                            number += "2";
+                            Console.Write("2");
+                            break;
+                        default:
+                            Console.WriteLine("Something went wrong.");
+                            break;
+
+                    }
+                }
+                Console.WriteLine();
+                if(number.Length != 4)
+                {
+                    throw new Exception();
+                }
+                counter += int.Parse(number);
+                number = "";
+            }
+            Console.WriteLine("Part 2 - " + counter);
         }
     }
 }
