bd370883055a48b5aa4de896b18bee8e41985631
Harry Adams <harry.adams@aberinstruments.com>
2021-12-10 14:37:31 +0000

    2021 Day 10

--- AdventOfCode2021/Day10.cs
+++ AdventOfCode2021/Day10.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,105 @@ namespace AdventOfCode2021
     {
         public static void Run()
         {
+            List<string> input = File.ReadAllLines("Day10Input.txt").ToList();
+            List<string> p2Input = new List<string>(input);
 
+            int errorCount = 0;
+            foreach(var line in input)
+            {
+                Stack<char> opens = new Stack<char>();
+                foreach (char c in line)
+                {
+                    bool valid = true;
+                    if(c == '(' || c == '[' || c == '{' || c == '<')
+                    {
+                        opens.Push(c);
+                        continue;
+                    }
+                    switch (c)
+                    {
+                        case ')':
+                            if(opens.Pop() != '(')
+                            {
+                                valid = false;
+                                errorCount += 3;
+                            }
+                            break;
+                        case ']':
+                            if (opens.Pop() != '[')
+                            {
+                                valid = false;
+                                errorCount += 57;
+                            }
+                            break;
+                        case '}':
+                            if (opens.Pop() != '{')
+                            {
+                                valid = false;
+                                errorCount += 1197;
+                            }
+                            break;
+                        case '>':
+                            if (opens.Pop() != '<')
+                            {
+                                valid = false;
+                                errorCount += 25137;
+                            }
+                            break;
+                    }
+                    if (!valid)
+                    {
+                        p2Input.Remove(line);
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Part 1 - " + errorCount);
+
+            List<char> openers = new List<char>() { '(', '[', '{', '<' };
+            List<char> closers = new List<char>() { ')', ']', '}', '>' };
+
+            List<long> scores = new List<long>();
+
+            foreach (var item in p2Input)
+            {
+                long score = 0;
+                Stack<char> opens = new Stack<char>();
+                foreach (char c in item)
+                {
+                    if (openers.Contains(c))
+                    {
+                        opens.Push(c);
+                    } else
+                    {
+                        _ = opens.Pop();
+                    }
+                }
+
+                foreach(var open in opens)
+                {
+                    switch (open)
+                    {
+                        case '(':
+                            score = (score * 5) + 1;
+                            break;
+                        case '[':
+                            score = (score * 5) + 2;
+                            break;
+                        case '{':
+                            score = (score * 5) + 3;
+                            break;
+                        case '<':
+                            score = (score * 5) + 4;
+                            break;
+                    }
+                }
+                scores.Add(score);
+            }
+
+            scores.Sort();
+            Console.WriteLine("Part 2 - " + scores[scores.Count() / 2]);
         }
     }
 }
