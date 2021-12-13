d98a54a802609a51d47aae1f8ce4e6cb03afdd46
Harry Adams <harry.adams@aberinstruments.com>
2021-12-13 15:19:58 +0000

    2021 Day 12 + 13

--- AdventOfCode2021/Day12.cs
+++ AdventOfCode2021/Day12.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,102 @@ namespace AdventOfCode2021
     {
         public static void Run()
         {
+            List<Cave> caves = new List<Cave>();
+            var inputs = File.ReadAllLines("Day12Input.txt");
+            foreach(var item in inputs)
+            {
+                var split = item.Split('-');
+                Cave startingCave = caves.Any(s => s.Name.Equals(split[0])) ? caves.Where(s => s.Name.Equals(split[0])).Single() : new Cave(split[0]);
+                if (!caves.Contains(startingCave)) caves.Add(startingCave);
 
+                Cave finishingCave = caves.Any(s => s.Name.Equals(split[1])) ? caves.Where(s => s.Name.Equals(split[1])).Single() : new Cave(split[1]);
+                if (!caves.Contains(finishingCave)) caves.Add(finishingCave);
+                startingCave.LinkedCaves.Add(finishingCave);
+                finishingCave.LinkedCaves.Add(startingCave);
+            }
+
+            Console.WriteLine("Part 1 - " + ExploreCavesPt1(caves.Where(s => s.Name.Equals("start")).Single(), new List<Cave>()));
+            Console.WriteLine("Part 2 - " + ExploreCavesPt2(caves.Where(s => s.Name.Equals("start")).Single(), new List<Cave>()));
+        }
+
+        public static int ExploreCavesPt1(Cave startingCave, List<Cave> route)
+        {
+            List<Cave> newRoute = new List<Cave>(route);
+            newRoute.Add(startingCave);
+            int sum = 0;
+            foreach(var cave in startingCave.LinkedCaves)
+            {
+                if (cave != startingCave) {
+                    if (route.Contains(cave) && !cave.IsLarge)
+                    {
+                        continue;
+                    }
+                    if (cave.Name.Equals("end"))
+                    {
+                        sum++;
+                    } else
+                    {
+                        sum += ExploreCavesPt1(cave, newRoute);
+                    }
+                }
+            }
+            return sum;
+        }
+
+        public static int ExploreCavesPt2(Cave startingCave, List<Cave> route)
+        {
+            List<Cave> newRoute = new List<Cave>(route);
+            newRoute.Add(startingCave);
+
+            int sum = 0;
+            foreach (var cave in startingCave.LinkedCaves)
+            {
+                if (cave != startingCave)
+                {
+                    if (cave.Name.Equals("start"))
+                    {
+                        //Can't reenter the start.
+                        continue;
+                    }
+                    if (!cave.IsLarge)
+                    {
+                        bool twoVisits = false;
+                        foreach(var visitedCave in newRoute)
+                        {
+                            if(newRoute.Where(s => !s.IsLarge && s == visitedCave).Count() == 2)
+                            {
+                                //Already visited a small cave twice.
+                                twoVisits = true;
+                                break;
+                            }
+                        }
+                        if(twoVisits && newRoute.Contains(cave)) continue;
+                    }
+                    if (cave.Name.Equals("end"))
+                    {
+                        sum++;
+                    }
+                    else
+                    {
+                        sum += ExploreCavesPt2(cave, newRoute);
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+
+    public class Cave
+    {
+        public string Name;
+        public bool IsLarge;
+        public List<Cave> LinkedCaves;
+
+        public Cave(string name)
+        {
+            LinkedCaves = new List<Cave>();
+            Name = name;
+            IsLarge = name.All(c => char.IsUpper(c)); 
         }
     }
 }
--- AdventOfCode2021/Day13.cs
+++ AdventOfCode2021/Day13.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,87 @@ namespace AdventOfCode2021
     {
         public static void Run()
         {
+            List<Coordinate> points = new List<Coordinate>();
+            var input = File.ReadAllLines("Day13Input.txt").ToList();
 
+            while (!string.IsNullOrWhiteSpace(input.First()))
+            {
+                var split = input.First().Split(',');
+                points.Add(new Coordinate(int.Parse(split[0]), int.Parse(split[1])));
+                input.RemoveAt(0);
+            }
+            input.RemoveAt(0);
+
+            for(int i = 0; i < input.Count(); i++)
+            {
+                var toDo = input[i].Split(' ')[2].Split('=');
+                int foldLine = int.Parse(toDo[1]);
+
+                if (toDo[0].Equals("x"))
+                {
+                    foreach (var coord in points)
+                    {
+                        if (coord.X == foldLine)
+                        {
+                            Console.WriteLine("Fuck");
+                        }
+
+                        if (coord.X > foldLine)
+                        {
+                            coord.X = foldLine - (coord.X - foldLine);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var coord in points)
+                    {
+                        if (coord.Y == foldLine)
+                        {
+                            Console.WriteLine("Fuck");
+                        }
+
+                        if (coord.Y > foldLine)
+                        {
+                            coord.Y = foldLine - (coord.Y - foldLine);
+                        }
+                    }
+                }
+
+                points = points.Distinct().ToList();
+                points = points.Where(x => x.X >= 0 && x.Y >= 0).ToList();
+                if (i == 0)
+                {
+                    Console.WriteLine("Part 1 - " + points.Count());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Part 2: ");
+            Console.WriteLine();
+
+            int maxX = 0;
+            int maxY = 0;
+            foreach (var point in points)
+            {
+                if (point.X > maxX) maxX = point.X;
+                if (point.Y > maxY) maxY = point.Y;
+            }
+
+            for(int i = 0; i <= maxY; i++)
+            {
+                for(int j = 0; j <= maxX; j++)
+                {
+                    if(points.Any(x => x.X == j && x.Y == i))
+                    {
+                        Console.Write("█");
+                    } else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
