dba141f1fc599995ce433591da0339f4c828bdcd
Harry Adams <hyperionharry@gmail.com>
2022-12-04 12:56:05 +0000

    2022 day 3 + 4

--- AdventOfCode2022/Day03.cs
+++ AdventOfCode2022/Day03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,47 @@ namespace AdventOfCode2022
     {
         public static void Run()
         {
+            int p1Sum = 0;
+            int p2Sum = 0;
+            int counter = 0;
+            List<string> group = new List<string>();
 
+            foreach (string line in File.ReadLines("Day03Input.txt"))
+            {
+                counter++;
+                group.Add(line);
+
+                var first = line.Substring(0, (int)(line.Length / 2));
+                var last = line.Substring((int)(line.Length / 2), (int)(line.Length / 2));
+
+                var common = first.Intersect(last);
+                foreach(char c in common)
+                {
+                    p1Sum += GetValue(c);
+                }
+
+                if(counter == 3)
+                {
+                    common = (group[0].Intersect(group[1])).Intersect(group[2]);
+                    foreach (char c in common)
+                    {
+                        p2Sum += GetValue(c);
+                    }
+
+                    counter = 0;
+                    group = new List<string>();
+                }
+            }
+            Console.WriteLine("Part 1 - " + p1Sum);
+            Console.WriteLine("Part 2 - " + p2Sum);
+        }
+
+        private static int GetValue(char c)
+        {
+            if (char.IsUpper(c))
+                return c - 38;
+            else
+                return c - 96;
         }
     }
 }
--- AdventOfCode2022/Day04.cs
+++ AdventOfCode2022/Day04.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,35 @@ namespace AdventOfCode2022
     {
         public static void Run()
         {
+            int p1Sum = 0;
+            int p2Sum = 0;
+            foreach (string line in File.ReadLines("Day04Input.txt"))
+            {
+                var pairs = line.Split(',');
+                var one = pairs[0].Split('-');
+                var two = pairs[1].Split('-');
 
+                var oneStart = int.Parse(one[0]);
+                var twoStart = int.Parse(two[0]);
+                var oneFin = int.Parse(one[1]);
+                var twoFin = int.Parse(two[1]);
+
+                if((oneStart >= twoStart && oneFin <= twoFin)
+                    || (twoStart >= oneStart && twoFin <= oneFin))
+                {
+                    p1Sum++;
+                }
+                
+                if((oneStart >= twoStart && oneStart <= twoFin)
+                    || (oneFin >= twoStart && oneFin <= twoFin)
+                    || (twoStart >= oneStart && twoStart <= oneFin)
+                    || (twoFin >= oneStart && twoFin <= oneFin))
+                {
+                    p2Sum++;
+                }
+            }
+            Console.WriteLine("Part 1 - " + p1Sum);
+            Console.WriteLine("Part 2 - " + p2Sum);
         }
     }
 }
