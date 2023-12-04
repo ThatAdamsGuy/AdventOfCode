9407c164bf9dc4fba8dfb46c27a744cd6469ffeb
Harry Adams <hyperionharry@gmail.com>
2023-12-04 13:29:39 +0000

    Day 3 P1, Day 4 Complete

--- AdventOfCode2023/Day03.cs
+++ AdventOfCode2023/Day03.cs
@@ -11,7 +11,136 @@ namespace AdventOfCode2023
     {
         public static void Run()
         {
+            List<string> log = new List<string>();
 
+            var input = File.ReadAllLines("Day03Input.txt");
+            var len = input.First().Length;
+            char[,] grid = new char[input.Length, len];
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                for(int j = 0; j < len; j++)
+                {
+                    grid[i,j] = (input[i])[j];
+                }
+            }
+
+            int sum = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                string curString = "";
+                bool bordersSymbol = false;
+                bool isAsterisk = false;
+                for(int j =0; j < len; j++)
+                {
+                    if(grid[i,j] < 48 || grid[i,j] > 57)
+                    {
+                        if(curString != "")
+                        {
+                            // Come to the end of number. Check this column, then sum
+                            if (i != 0)
+                            {
+                                // Not first row, check above
+                                bordersSymbol |= IsSymbol(grid[i - 1, j]);
+                            }
+
+                            // Check centre
+                            bordersSymbol |= IsSymbol(grid[i, j]);
+
+                            if (i != input.Count() - 1)
+                            {
+                                // Not final row, check below
+                                bordersSymbol |= IsSymbol(grid[i + 1, j]);
+                            }
+
+                            log.Add(curString);
+                            if (bordersSymbol)
+                            {
+                                sum += int.Parse(curString);
+                                log.Add("Adjacent");
+                            }
+                            curString = "";
+                            bordersSymbol = false;
+                        }
+                        continue;
+                    }
+                    
+                    // We have a digit!
+                    if(curString == "")
+                    {
+                        // First digit of new number
+                        if(j != 0)
+                        {
+                            // Not first column, check left column
+                            if (i != 0)
+                            {
+                                // Not first row, check above and left
+                                bordersSymbol |= IsSymbol(grid[i - 1, j - 1]);
+                            }
+
+                            // Check left
+                            bordersSymbol |= IsSymbol(grid[i, j - 1]);
+
+                            if (i != input.Count() - 1)
+                            {
+                                // Not final row, check below and left
+                                bordersSymbol |= IsSymbol(grid[i + 1, j - 1]);
+                            }
+
+                            // check above and below
+                            if (i != 0) bordersSymbol |= IsSymbol(grid[i - 1, j]);
+                            if (i != len - 1) bordersSymbol |= IsSymbol(grid[i + 1, j]);
+                        }
+                        else
+                        {
+                            // First column, just check above and below
+                            if(i != 0) bordersSymbol |= IsSymbol(grid[i - 1, j]);
+                            if(i != len - 1) bordersSymbol |= IsSymbol(grid[i + 1, j]);
+                        }
+                    }
+                    else if (j == len - 1)
+                    {
+                        // Final column, check above and below then sum
+                        if (i != 0) bordersSymbol |= IsSymbol(grid[i - 1, j]);
+                        if (i != len - 1) bordersSymbol |= IsSymbol(grid[i + 1, j]);
+
+                        curString += grid[i, j];
+                        log.Add(curString);
+                        if (bordersSymbol)
+                        {
+                            sum += int.Parse(curString);
+                            log.Add("Adjacent");
+                        }
+                        curString = "";
+                        bordersSymbol = false;
+                    }
+                    else
+                    {
+                        //Middle of the grid
+                        if (i != 0)
+                        {
+                            // Not first row, check above
+                            bordersSymbol |= IsSymbol(grid[i - 1, j]);
+                        }
+
+                        if (i != input.Count() - 1)
+                        {
+                            // Not final row, check below
+                            bordersSymbol |= IsSymbol(grid[i + 1, j]);
+                        }
+                    }
+
+                    curString += grid[i, j];
+                }
+            }
+            File.WriteAllLines(@"C:\Users\harry\Desktop\log.txt", log);
+
+            Console.WriteLine("Part 1 - " + sum);
+        }
+
+        private static bool IsSymbol(char c)
+        {
+            return ((c < 48 || c > 57) && c != '.');
         }
     }
 }
--- AdventOfCode2023/Day04.cs
+++ AdventOfCode2023/Day04.cs
@@ -11,7 +11,54 @@ namespace AdventOfCode2023
     {
         public static void Run()
         {
+            var input = File.ReadAllLines("Day04Input.txt");
+            int sum = 0;
+            foreach(var line in input)
+            {
+                var numbers = line.Split(':')[1].Split('|');
+                var winningNumbers = numbers[0].Trim().Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToList();
+                var cardNumbers = numbers[1].Trim().Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToList();
 
+                sum += (int)Math.Pow(2, (winningNumbers.Intersect(cardNumbers).Count() - 1));
+            }
+            Console.WriteLine("Part 1 - " + sum);
+
+            Dictionary<int, int> cards = new Dictionary<int, int>();
+
+            foreach (var line in input)
+            {
+                var key = int.Parse(line.Split(':')[0].Split(' ').Last());
+
+                if (cards.ContainsKey(key))
+                {
+                    cards[key]++;
+                }
+                else
+                {
+                    cards.Add(key, 1);
+                }
+
+                var numbers = line.Split(':')[1].Split('|');
+                var winningNumbers = numbers[0].Trim().Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToList();
+                var cardNumbers = numbers[1].Trim().Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToList();
+
+                int winnings = winningNumbers.Intersect(cardNumbers).Count();
+                for (int j = 0; j < cards[key]; j++)
+                {
+                    for (int i = 1; i <= winnings; i++)
+                    {
+                        if (cards.ContainsKey(key + i))
+                        {
+                            cards[key + i]++;
+                        }
+                        else
+                        {
+                            cards.Add(key + i, 1);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Part 2 - " + cards.Sum(x => x.Value));
         }
     }
 }
